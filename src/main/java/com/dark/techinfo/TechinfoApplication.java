b09d50c51bbd845dcb5bb104ec75ff5f0f6e1edc
Sajib Ahmed <sajibprime3@gmail.com>
2025-01-20 06:02:35 -0500

    Spring init.

---
+++ src/main/java/com/dark/techinfo/TechinfoApplication.java
@@ -0,0 +1,13 @@
+package com.dark.techinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dark/techinfo/TechinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dark.techinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
